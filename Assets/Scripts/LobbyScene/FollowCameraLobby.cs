523f9b4bdb1572d5a58d16f8c8808897037bb02b
jaewoo0530 <6672jjw@gmail.com>
2025-10-23 16:16:12 +0900

    Add: 로비씬 플레이어 추격 카메라

--- Assets/Scripts/LobbyScene/FollowCameraLobby.cs
+++ Assets/Scripts/LobbyScene/FollowCameraLobby.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+public class FollowCameraLobby : MonoBehaviour
 {
     public Transform target;
     float offsetX;
+    float offsetY;
 
     void Start()
     {
@@ -13,6 +14,7 @@ public class FollowCamera : MonoBehaviour
             return;
 
         offsetX = transform.position.x - target.position.x;
+        offsetY = transform.position.y - target.position.y;
     }
 
     void Update()
@@ -22,6 +24,7 @@ public class FollowCamera : MonoBehaviour
 
         Vector3 pos = transform.position;
         pos.x = target.position.x + offsetX;
+        pos.y = target.position.y + offsetY;
         transform.position = pos;
     }
 }
