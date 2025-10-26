ca5a74c9c2734ee939658b2a558a62c1b667795c
jaewoo0530 <6672jjw@gmail.com>
2025-10-26 19:22:25 +0900

    Add: 임시코드

--- Assets/Scripts/TopDown/Entity/PlayerController.cs
+++ Assets/Scripts/TopDown/Entity/PlayerController.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : BaseController
 {
+    private TopDownManager gameManager;
     private Camera camera;
 
-    protected override void Start()
+    public void Init(TopDownManager gameManager)
     {
-        base.Start();
+        this.gameManager = gameManager;
         camera = Camera.main;
     }
+
 
     protected override void HandleAction()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertial = Input.GetAxisRaw("Vertical");
-        movementDirection = new Vector2(horizontal, vertial).normalized;
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
 
         Vector2 mousePosition = Input.mousePosition;
         Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
